78a93f9a8037c1a47b7442b39e6baf1265d8d162
sumi <121598930+sumicoder@users.noreply.github.com>
2025-07-22 20:51:01 +0900

    ログイン認証まで追加

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "awsamplifyapp0257b1db": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+// AWS Amplify UIのスタイルをインポート
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+// aws-exportsファイルをインポートし、Amplifyを設定
+import awsExports from './aws-exports.js';
+Amplify.configure(awsExports);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <h3>Hello World</h3>
-    </>
-  )
+// アプリのスタイルをインポート
+import './App.css';
+
+function App({ user, signOut }: { user: any, signOut: () => void }) {
+    return (
+        <>
+            {user ? (
+                <>
+                    <h3>Hello {user.username}</h3>
+                    <button onClick={signOut}>Sign Out</button>
+                </>
+            ) : (
+                <h3>Please sign in</h3>
+            )}
+        </>
+    );
 }
 
-export default App
+export default withAuthenticator(App);
